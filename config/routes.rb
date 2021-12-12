f5d65277bae01e10786f432ac1b282e408c58c4f
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2021-12-12 21:02:12 +0700

    first

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+  get 'home/index'
+  resources :meetings do
+    resources :comments
+  end
+  devise_for :users
+  get 'active-sessions', to: "meetings#active_sessions"
+  root to: 'home#index'
+  resources :users, only: [:index]
+end
